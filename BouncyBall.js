f3c7847bd6adff5d8232e63c3e8f42d19dda0528
sadhanananna <32163265+sadhanananna@users.noreply.github.com>
2021-09-27 19:43:21 +0530

    Update BouncyBall.js

--- BouncyBall.js
+++ BouncyBall.js
@@ -1,53 +1,53 @@
 class BouncyBall{
 	constructor(x,y,r)
 	{
-		//var options={
-		//restitution:0.3
-		//friction:5
-		//	density:1
-		//}
+		// var options={
+		// 	restitution:0.3
+		// 	friction:5
+		// 	density:1
+		// }
 
 		// var options={
-	//	restitution:5,
-		//	friction:5
-			//density:20
-		//}
+		// 	restitution:5,
+		// 	friction:5
+		// 	density:20
+		// }
 
-		 var options={
-	    restitution:0.3,
+		var options={
+			restitution:0.3,
 			friction:5,
-		density:1
-	  }
+			density:1
+		}
 
-		//var options{
-		 	//restitution:5,
-		 //	friction:5,
-		//density:20
-		 // }
+		// var options{
+		// 	restitution:5,
+		// 	friction:5,
+		// 	density:20
+		// }
 
 
 		this.x=x;
 		this.y=y;
 		this.r=r
 		// this.body=Bodies.ellipse(this.x, this.y, (this.r-20)/2, options)
-	//	World.add(world, this.body);
+		// World.add(world, this.body);
 
 		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
-		 World.add(world, this.body);
+		World.add(world, this.body);
 
-		//this.body=Bodies.rectangle(this.x, this.y, (this.r-20)/2, options)
-		 //World.add(world, this.body);
+		// this.body=Bodies.rectangle(this.x, this.y, (this.r-20)/2, options)
+		// World.add(world, this.body);
 
 		
-		//this.body=Bodies(this.x, this.y, (this.r-20)/2, options)
+		// this.body=Bodies(this.x, this.y, (this.r-20)/2, options)
 		// World.add(world, this.body);
 
 	}
 	display()
 	{
-		    var bouncyBallPos=this.body.position;		
-		    push()
-		    translate(bouncyBallPos.x, bouncyBallPos.y);
+			var bouncyBallPos=this.body.position;		
+			push()
+			translate(bouncyBallPos.x, bouncyBallPos.y);
 			rectMode(CENTER)
 			strokeWeight(4);
 			stroke("black");
@@ -55,7 +55,7 @@ class BouncyBall{
 			ellipse(0,0,this.r, this.r);
 			// circle(0,0,this.r, this.r);
 			// rect(0,0,this.r, this.r);
-			//body(0,0,this.r, this.r);
+			// body(0,0,this.r, this.r);
 			pop()
 	}
 
